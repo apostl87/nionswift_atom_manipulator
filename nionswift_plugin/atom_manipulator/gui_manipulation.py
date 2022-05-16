81d428409bc30f44f11663beba0c5dff2bc99e2a
Andreas Postl <andreas.postl@univie.ac.at>
2022-05-16 23:21:48 +0200

    renamed main object

--- nionswift_plugin/atom_manipulator/gui_manipulation.py
+++ nionswift_plugin/atom_manipulator/gui_manipulation.py
@@ -13,9 +13,9 @@ defaults = {'snapshot': True}
 
 class ManipulationModule(lib_utils.AtomManipulatorModule):
 
-    def __init__(self, ui, api, document_controller, manipulator_object=None):
+    def __init__(self, ui, api, document_controller, manipulator=None):
         super().__init__(ui, api, document_controller)
-        self.manip_obj = manipulator_object
+        self.manipulator = manipulator
         self.snapshot = None
         self.stop_auto_manipulate_event = threading.Event()
 
@@ -43,36 +43,36 @@ class ManipulationModule(lib_utils.AtomManipulatorModule):
             if automanip_button.state:
                 logging.info(lib_utils.log_message("Starting automatical manipulation ..."))
                 ## Init
-                self.manip_obj.tractor_beam_module.rdy.set()
-                self.manip_obj.path_finding_module.rdy.clear()
-                self.manip_obj.structure_recognition_module.rdy.clear()
+                self.manipulator.tractor_beam_module.rdy.set()
+                self.manipulator.path_finding_module.rdy.clear()
+                self.manipulator.structure_recognition_module.rdy.clear()
                 self.stop_auto_manipulate_event.clear()
                 
                 ## Call modules
                 # Structure recognition.
-                if self.snapshot: self.manip_obj.snapshot_counter = 1
-                else: self.manip_obj.snapshot_counter = None
-                lib_structure_recognition.analyze_and_show(self.manip_obj.structure_recognition_module, auto_manipulate=True)
+                if self.snapshot: self.manipulator.snapshot_counter = 1
+                else: self.manipulator.snapshot_counter = None
+                lib_structure_recognition.analyze_and_show(self.manipulator.structure_recognition_module, auto_manipulate=True)
 
                 # Pathfinding.
-                lib_pathfinding.find_paths(self.manip_obj, auto_manipulate=True)
+                lib_pathfinding.find_paths(self.manipulator, auto_manipulate=True)
                 
                 # Tractor beam.
                 try:
                     # Communication events.
-                    comm_events = [self.manip_obj.structure_recognition_module.new_image,
-                                   self.manip_obj.path_finding_module.rdy,
-                                   self.manip_obj.tractor_beam_module.rdy]
+                    comm_events = [self.manipulator.structure_recognition_module.new_image,
+                                   self.manipulator.path_finding_module.rdy,
+                                   self.manipulator.tractor_beam_module.rdy]
                     self.fb_obj = fb.ADFFeedbackDelegate(self.api, 
-                        offline_test_mode = self.manip_obj.tractor_beam_module.otm_check_box.checked,
-                        autodetect_changes = self.manip_obj.tractor_beam_module.adc_check_box.checked,
+                        offline_test_mode = self.manipulator.tractor_beam_module.otm_check_box.checked,
+                        autodetect_changes = self.manipulator.tractor_beam_module.adc_check_box.checked,
                         external_stop_event = self.stop_auto_manipulate_event)
-                    self.fb_obj.startmap(self.manip_obj.tractor_beam_module.frame_timeout,
-                                         self.manip_obj.tractor_beam_module.jump_threshold,
-                                         self.manip_obj.tractor_beam_module.drift_threshold,
-                                         self.manip_obj.tractor_beam_module.reposition_timeout,
+                    self.fb_obj.startmap(self.manipulator.tractor_beam_module.frame_timeout,
+                                         self.manipulator.tractor_beam_module.jump_threshold,
+                                         self.manipulator.tractor_beam_module.drift_threshold,
+                                         self.manipulator.tractor_beam_module.reposition_timeout,
                                          comm_events,
-                                         self.manip_obj)
+                                         self.manipulator)
                 except Exception as exc:
                     logging.info(lib_utils.log_message("Exception when calling adf_feedback"))
                     print(exc)
