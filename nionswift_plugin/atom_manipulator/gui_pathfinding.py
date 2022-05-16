8f3750c166d99645a3aa39e8319e79e8428c41d0
Andreas Postl <andreas.postl@univie.ac.at>
2022-05-17 00:12:14 +0200

    Completed commentation. Unificatiion

--- nionswift_plugin/atom_manipulator/gui_pathfinding.py
+++ nionswift_plugin/atom_manipulator/gui_pathfinding.py
@@ -26,7 +26,21 @@ class PathFindingModule(AtomManipulatorModule):
         self.section = Section(self.ui, 'Pathfinding')
         column.add(self.section)
 
-        # Buttons for adding and removing foreign atoms and target sites.
+        # Callback functions.
+        def add_foreign_atoms_clicked():
+            add_remove_atoms_sites(0)   
+        def remove_foreign_atoms_clicked():
+            add_remove_atoms_sites(1)
+        def add_target_sites_clicked():
+            add_remove_atoms_sites(2)
+        def remove_target_sites_clicked():
+            add_remove_atoms_sites(3)
+
+        #### GUI elements.
+
+        ## Adding, removing, and displaying foreign atoms and target sites.
+        
+        # Toggle text of buttons.
         def toggle_button1_text(): # Foreign atoms
             while True:
                 yield _('Add')
@@ -44,16 +58,8 @@ class PathFindingModule(AtomManipulatorModule):
                 yield _('Remove')
                 yield _('Stop removing')
         buttons_text = [toggle_button1_text(), toggle_button2_text(), toggle_button3_text(), toggle_button4_text()]
-         
-        def add_foreign_atoms_clicked():
-            add_remove_atoms_sites(0)   
-        def remove_foreign_atoms_clicked():
-            add_remove_atoms_sites(1)
-        def add_target_sites_clicked():
-            add_remove_atoms_sites(2)
-        def remove_target_sites_clicked():
-            add_remove_atoms_sites(3)
  
+        # Button widgets.
         self.add_foreign_atoms_button = self.ui.create_push_button_widget(next(buttons_text[0]))
         self.add_foreign_atoms_button.state = False
         self.add_foreign_atoms_button.on_clicked = add_foreign_atoms_clicked
@@ -72,7 +78,12 @@ class PathFindingModule(AtomManipulatorModule):
         self.add_remove_buttons.append(self.remove_foreign_atoms_button)
         self.add_remove_buttons.append(self.add_target_sites_button)
         self.add_remove_buttons.append(self.remove_target_sites_button)
-            
+
+        # Label widgets.
+        self.N_foreign_atoms_label = self.ui.create_label_widget('0')
+        self.N_target_sites_label = self.ui.create_label_widget('0')
+
+
         def add_remove_atoms_sites(button_idx):
             last_mode = np.where(list(map(lambda x: x.state, self.add_remove_buttons)))[0]
             if last_mode.size > 0 and button_idx != last_mode[0]:
@@ -82,11 +93,7 @@ class PathFindingModule(AtomManipulatorModule):
             self.add_remove_buttons[button_idx].state = not self.add_remove_buttons[button_idx].state
             lib_pathfinding.add_or_remove_foreign_atoms_or_target_sites(self.manipulator, mode=button_idx, startstop=self.add_remove_buttons[button_idx].state)
 
-        # Labels.
-        self.N_foreign_atoms_label = self.ui.create_label_widget('0')
-        self.N_target_sites_label = self.ui.create_label_widget('0')
-        
-        # GUI rows for foreign atoms and target sites.
+        # Row widgets.
         foreign_atoms_row = self.ui.create_row_widget()
         foreign_atoms_row.add(self.ui.create_label_widget(_('Foreign atoms: ')))
         foreign_atoms_row.add(self.N_foreign_atoms_label)
@@ -101,7 +108,7 @@ class PathFindingModule(AtomManipulatorModule):
         target_sites_row.add(self.remove_target_sites_button)
         target_sites_row.add_stretch()
         
-        # Other GUI rows.
+        ## Other rows.
         max_bond_length_row, self.max_bond_length_line_edit = line_edit_template(self.ui, 'Max. bond length [A]')
         def max_bond_length_editing_finished(text):
             if len(text) > 0:
@@ -126,6 +133,7 @@ class PathFindingModule(AtomManipulatorModule):
         # Set defaults.
         max_bond_length_editing_finished(str(defaults['max_bond_length']))
         
+        # Assemble GUI elements.
         self.section.column.add(foreign_atoms_row)
         self.section.column.add(target_sites_row)
         self.section.column.add(max_bond_length_row)
