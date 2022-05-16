63d69f3c46b8b87bff4f8bb85f2e4348ba9202ba
Andreas Postl <andreas.postl@univie.ac.at>
2022-05-16 21:36:03 +0200

    1.1.build_1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ import os
 
 setuptools.setup(
     name="nionswift_atom_manipulator",
-    version="1.1",
+    version="1.1.build_1",
     author="Andreas Postl",
     author_email="andreas.postl@univie.ac.at",
     description= "A Nion Swift plug-in for single-atom manipulation using a STEM electron beam",
     packages=["nionswift_plugin.atom_manipulator", "nionswift_plugin.atom_manipulator.classes"],
-    install_requires=[], # see requirements.txt
+    install_requires=[], # see requirements.txt and gui_main.py
     license='GPLv3',
     classifiers=[
         "Development Status :: 1 - Alpha",
